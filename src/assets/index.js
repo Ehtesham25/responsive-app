8645d5716bcfc1a0dd367334eafbfd41bf5ba1b2
ehtesham25 <ehteshamulhaqkhan25@gmail.com>
2022-07-02 06:03:43 +0500

    index assets

---
+++ src/assets/index.js
@@ -0,0 +1,48 @@
+import facebook from "./icons/facebook.svg"
+import twitter from "./icons/twitter.svg"
+import google from "./icons/google.svg"
+import email from "./icons/email.svg"
+import instagram from "./icons/instagram.svg"
+import user from "./icons/user-icon.svg"
+import cart from "./icons/cart.svg"
+import edit from "./icons/edit.svg"
+import phone from "./icons/phone.svg"
+import clock from "./icons/clock.svg"
+import logo from "./icons/logo.svg"
+import logo2 from "./icons/logo-2.svg"
+import shadow from "./icons/shadow.svg"
+import hr from "./icons/hr.svg"
+import starFilled from "./icons/start-filled.svg"
+
+// images
+import hero from "./images/hero.png"
+import straw from "./images/straw.png"
+import plate from "./images/plate-1.png"
+import plate2 from "./images/plate-2.png"
+import plate3 from "./images/plate-3.png"
+
+export const icons= {
+  facebook,
+  twitter,
+  google,
+  email,
+  instagram,
+  user,
+  cart,
+  edit,
+  phone,
+  clock,
+  logo,
+  logo2,
+  shadow,
+  hr,
+  starFilled
+}
+
+export const images={
+  hero,
+  straw,
+  plate,
+  plate2,
+  plate3
+}
